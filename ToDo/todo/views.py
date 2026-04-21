e7c36821c3081daab23c25631d65644e50258507
jibin <jibinkrishna0010@gmail.com>
2026-04-21 12:31:14 +0530

    Make edit and delete button

--- ToDo/todo/views.py
+++ ToDo/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, get_object_or_404 #type: ignore
+from django.shortcuts import redirect, get_object_or_404, render #type: ignore
 from .models import Task
 from django.http import HttpResponse #type: ignore
 
@@ -18,4 +18,22 @@ def mark_as_undone(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.is_completed = False
     task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task' : get_task,
+        }
+        return render(request, 'edit_task.html', context)
+    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
