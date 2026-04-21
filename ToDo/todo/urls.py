596ef5a75cd9369f7e92196b03548648fbd445a4
jibin <jibinkrishna0010@gmail.com>
2026-04-21 11:42:06 +0530

    Make mark as done and mark as undone button

--- ToDo/todo/urls.py
+++ ToDo/todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path #type: ignore
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
 ]
